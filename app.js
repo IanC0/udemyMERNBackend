6eb6c744357fd43a0ea39c74283e7544bd5abdf6
Ian Connell <ian10gt@gmail.com>
2023-10-30 21:24:17 +0000

    92: Getting a Place By User ID

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 const placesRoutes = require('./routes/places-routes');
 
-
+const userRoutes = require('./routes/user-routes')
 
 const app = express();
 
 app.use('/api/places', placesRoutes);
 
-app.listen(5000)
+app.listen(5000);
