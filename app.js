e54e848b6ca162b340b106796ed94cf20bd45b5e
Ian Connell <ian10gt@gmail.com>
2023-12-14 17:52:09 +0000

    96: Adding a POST Route & Using Postman

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const userRoutes = require('./routes/user-routes')
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/places', placesRoutes);
 
 app.use((error, req, res, next) => {
--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,3 +1,5 @@
+const uuid = require('uuid').v4;
+
 const HttpError = require("../models/http-error")
 
 const DUMMY_PLACES = [
@@ -40,6 +42,24 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;  
+  
+  const createdPlace = {
+    id: uuid(), //1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator
+  }
+
+  DUMMY_PLACES.push(createdPlace);
+  res.status(201).json({'places': createdPlace})
+}
+
+
 //both bundled to one object
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
